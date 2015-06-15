aec161614d51cbd94b57add2165a6148479f586a
jchansen <jason_hansen@outlook.com>
2015-06-15 09:25:55 -0700

    filter test

--- test/specs/baseStore.spec.js
+++ test/specs/baseStore.spec.js
@@ -5,11 +5,11 @@ define(function(require) {
       BaseStore = require('stores/BetterBaseStore'),
       server;
 
-  before(function(){
+  beforeEach(function(){
     server = sinon.fakeServer.create();
   });
 
-  after(function(){
+  afterEach(function(){
     server.restore();
   });
 
@@ -33,29 +33,74 @@ define(function(require) {
 
     describe("#find", function(){
 
-      beforeEach(function(){
-        server.respondWith(
-          "GET",
-          "/api/tests",
-          [
-            200, {
-              "Content-Type": "application/json"
-            },
-            JSON.stringify([
-              { "id": 12, "comment": "Hey there" }
-            ])
-          ]);
+      describe("with no arguments", function(){
+
+        beforeEach(function(){
+          server.respondWith(
+            "GET",
+            "/api/tests",
+            [
+              200, {
+                "Content-Type": "application/json"
+              },
+              JSON.stringify([
+                { "id": 12, "comment": "Hey there" }
+              ])
+            ]);
+        });
+
+        it("should fetch models from server if none exist", function () {
+          //var collection = new TestCollection();
+          //expect(collection.url).to.equal("/api/tests");
+          //collection.fetch().done(function(){
+          //  expect(collection.length).to.equal(1);
+          //  done();
+          //});
+
+          var store = new TestStore();
+          var results = store.find();
+          expect(results).to.be.undefined;
+          server.respond();
+          results = store.find();
+          expect(results.length).to.equal(1);
+          expect(server.requests[0].url).to.equal("/api/tests");
+        });
       });
 
-      it("should fetch models from server if none exist", function (done) {
-        var collection = new TestCollection();
-        collection.fetch().done(function(){
-          console.log(collection.toJSON());
-          done();
+      describe("with arguments", function(){
+        var url = "/api/tests?name=hello";
+
+        beforeEach(function(){
+          server.respondWith(
+            "GET",
+            url,
+            [
+              200, {
+                "Content-Type": "application/json"
+              },
+              JSON.stringify([
+                { "id": 12, "comment": "Hey there" }
+              ])
+            ]);
         });
-        server.respond();
-        expect(collection.url).to.equal("/api/tests");
-      });
+
+        it.only("should pass filter parameters to the server", function(){
+          var store = new TestStore();
+          var options = {
+            where: {
+              name: "hello"
+            }
+          };
+
+          var results = store.find(options);
+          expect(results).to.be.undefined;
+          server.respond();
+          results = store.find(options);
+          expect(results.length).to.equal(1);
+          expect(server.requests[0].url).to.equal(url);
+        })
+      })
+
     });
 
   });
