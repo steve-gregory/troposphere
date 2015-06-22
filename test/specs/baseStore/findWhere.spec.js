09eec0a6e6849cc49f83a890eb7f5c4f908ead73
jchansen <jason_hansen@outlook.com>
2015-06-21 21:29:39 -0700

    more specs pass

--- test/specs/baseStore/findWhere.spec.js
+++ test/specs/baseStore/findWhere.spec.js
@@ -7,9 +7,27 @@ define(function(require) {
       server;
 
   describe("BaseStore#findWhere", function(){
+    var url = "/api/tests?name=hello";
 
     beforeEach(function(){
       server = sinon.fakeServer.create();
+
+      server.respondWith(
+        "GET",
+        url,
+        [
+          200, {
+            "Content-Type": "application/json"
+          },
+          JSON.stringify({
+            count: 1,
+            next: null,
+            previous: null,
+            results: [
+              {"id": 1, "name": "hello"}
+            ]
+          })
+        ]);
     });
 
     afterEach(function(){
@@ -17,27 +35,6 @@ define(function(require) {
     });
 
     describe("when query does not exist", function () {
-      var url = "/api/tests?name=hello";
-
-      beforeEach(function(){
-        server.respondWith(
-          "GET",
-          url,
-          [
-            200, {
-              "Content-Type": "application/json"
-            },
-            JSON.stringify({
-              count: 2,
-              next: null,
-              previous: null,
-              results: [
-                {"id": 1, "name": "hello"},
-                {"id": 2, "name": "world"}
-              ]
-            })
-          ]);
-      });
 
       it("should pass filter parameters to the server", function(){
         var store = new TestStore();
@@ -60,16 +57,18 @@ define(function(require) {
     });
 
     describe("when query does exist", function () {
-      var url = "/api/tests?name=hello";
+      var query = {
+            name: "hello"
+          },
+          store;
 
-      // todo: add data to store using dispatcher
+      beforeEach(function(){
+        store = new TestStore();
+        store.findWhere(query);
+        server.respond();
+      });
 
       it("should pass filter parameters to the server", function(){
-        var store = new TestStore();
-        var query = {
-          name: "hello"
-        };
-
         // query should now exist
         var results = store.findWhere(query);
         expect(results.length).to.equal(1);
